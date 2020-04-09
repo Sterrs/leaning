a1c7b354e160d1bf6afdc539dc017f4ed2d4a60c
Izaak van Dongen <izaak.van.dongen@gmail.com>
2020-04-10 00:05:59 +0100

    Prove some things about mod

--- src/principia/mynat/hcf.lean
+++ src/principia/mynat/hcf.lean
@@ -31,6 +31,9 @@ def mod := well_founded.fix lt_well_founded mod.F
 
 instance: has_mod mynat := ⟨mod⟩
 
+lemma mod_def_aux (x y : mynat) : x % y = if h : 0 < y ∧ y ≤ x then (x - y) % y else x :=
+congr_fun (well_founded.fix_eq lt_well_founded mod.F x) y
+
 variables {m n k : mynat}
 
 theorem mod_zero: ∀ {m : mynat}, m % 0 = m
@@ -41,9 +44,52 @@ theorem zero_mod: ∀ {m : mynat}, 0 % m = 0
 | zero     := rfl
 | (succ m) := rfl
 
-theorem zero_mod_iff_dvd: m % n = 0 ↔ m ∣ n :=
+theorem mod_cancel: (m + n) % n = m % n :=
 begin
-  sorry, -- Not a clue
+  rw mod_def_aux,
+  cases n, {
+    rw dif_neg _, {
+      rw [zz, mod_zero],
+      refl,
+    }, {
+      from (λ ⟨h, _⟩, lt_nrefl h),
+    },
+  }, {
+    rw dif_pos _, {
+      rw add_sub,
+    }, {
+      split, {
+        from zero_lt_succ,
+      }, {
+        rw add_comm,
+        from le_to_add,
+      }
+    }
+  }
+end
+
+theorem mod_cancel_lots: (m + k * n) % n = m % n :=
+begin
+  induction k with k' hk', {
+    rw [zz, zero_mul, add_zero],
+  }, {
+    rw [succ_mul, ←add_assoc, mod_cancel, hk'],
+  },
+end
+
+theorem zero_mod_iff_dvd: m % n = 0 ↔ n ∣ m :=
+begin
+  split, {
+    -- probably we'll need to define div before we can do this
+    sorry,
+  }, {
+    assume hnm,
+    cases hnm with k hk,
+    rw hk,
+    rw ←zero_add (k * n),
+    rw mod_cancel_lots,
+    rw zero_mod,
+  },
 end
 
 end hidden
