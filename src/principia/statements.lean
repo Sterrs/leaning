4639bb0686c12b9f814f6341b0e83846d0655d75
Alastair Horn <alastairthorn@outlook.com>
2020-04-10 21:17:57 +0100

    Add unfinished statement of Collatz conjecture

--- src/principia/statements.lean
+++ src/principia/statements.lean
@@ -1,6 +1,7 @@
 -- vim: ts=2 sw=0 sts=-1 et ai tw=70
 
 import .mynat.parity
+import .mynat.sum
 
 -- This file is specifically for statements of famous theorems.
 -- It is a useful and fun thing to state these, but they're better off
@@ -33,4 +34,20 @@ begin
 end
 
 theorem fermats_last_theorem (a b c n: mynat):
-a ≠ 0 → b ≠ 0 → a^n + b^n = c^n → n = 1 ∨ n = 2 := sorry
+a ≠ 0 → b ≠ 0 → a ^ n + b ^ n = c ^ n → n = 1 ∨ n = 2 := sorry
+
+def collatz_sequence : mynat → sequence mynat
+| m zero     := m
+| m (succ n) :=
+begin
+  have prev := collatz_sequence m n,
+  exact
+  if even prev then
+    sorry -- Need division
+  else
+    3 * prev + 1,
+end
+
+-- "Mathematics may not be ready for such problems"
+theorem collatz_conjecture:
+∀ m, ∃ n, collatz_sequence m n = 1 := sorry
